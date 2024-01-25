6f99111cde63dab24fe9d23cdb5e64e131094bac
charliegale16 <charlesgale@csu.fullerton.edu>
2024-01-25 03:44:50 -0800

    started contact half-way done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,22 @@ const Contact = () => {
   return (
     <div className='contact-container'>
         
-        <h1>Contact</h1>
-        
+            <div className='contact-header'>
+                <h1>Get in touch with me!</h1>
+                
+            </div>
+            <div className='contact-description'>
+              <p>Are you interested in collaboration? Feel free to reach out!</p>
+            </div>
+            <div className='contact-socials-container'>
+              <div className='contact-socials'>
+              <a href="https://www.github.com/charliegale16" target="_blank" rel="noreferrer"> <picture> <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/github.svg" alt="github" width="50" height="50" /> </picture> </a>
+              <a href="https://www.linkedin.com/in/charlesgale1" target="_blank" rel="noreferrer"> <picture> <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/linkedin-dark.svg" /> <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/linkedin.svg" /> <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/linkedin.svg" alt="linkedin" width="50" height="50" /> </picture> </a>
+                <a href="https://discord.com/users/chawwwa" target="_blank" rel="noreferrer"> <picture> <source media="(prefers-color-scheme: dark)" srcset="undefined" /> <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/discord.svg" /> <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/discord.svg" alt="discord" width="50" height="50" /> </picture> </a>
+                <h1>Email</h1>
+                <h1>Resume</h1>
+              </div>
+            </div>  
         </div>
   )
 }
