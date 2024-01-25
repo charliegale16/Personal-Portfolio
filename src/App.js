0cce7f7429c38e06b1806f6a308f448e62c0641e
charliegale16 <charles.gale@csu.fullerton.edu>
2024-01-24 23:49:07 -0800

    started contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/Nav';
 import Hero from './components/Hero';
 import Header from './components/Header';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
       <div className='Projects'>
       <Projects />
       </div>
-
+      <div className='Contact'>
+      <Contact />
+      </div>
 
     </div>
   );
